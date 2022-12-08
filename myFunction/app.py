ea6ea0dc6472cd27425fd1d1e5d72efb3e0c1575
Nguyen Smile <nguyenlc.smile@gmail.com>
2022-12-08 21:35:17 +0700

    dev

--- myFunction/app.py
+++ myFunction/app.py
@@ -3,11 +3,7 @@ from db import connectDB
 
 
 def index(event, context):
-    db = connectDB('AKIAQKOY52DIAY5KA76N',
-                   '+W9hRTcItgu9ACqKsRFshwR9tgnxRQWdqf+JDJep', 'myTable')
-    myTable = db.myTable()
-    print(myTable)
-
+   
     return {
         'statuscode': 200,
         'body': json.dumps('Hello HiHi!!!')
